4054e42f5ca53726b4e086e5f3eaaec7fc8b88dc
J2304789 <88808883+J2304789@users.noreply.github.com>
2021-10-11 23:00:29 -0400

    Created Chainlink Folder

---
+++ Chainlink/ChainlinklLottery.sol
@@ -0,0 +1,102 @@
+//This is for Educational Purposes Only
+pragma solidity ^0.8.8;
+import "https://github.com/smartcontractkit/chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+
+//Lottery contract inherits from the ChainlinkClient contract
+contract Lottery is ChainlinkClient {
+    
+        //Sets Chainlink Alarm clocks by connecting them to Kovan Testnet Oracles
+        //Kovan Testnet Website: https://kovan-testnet.github.io/website/
+        address Chainlink_Oracle_Alarm = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
+        bytes32 Chainlink_Alarm_JobId = "982105d690504c5d9ce374d040c08654";
+        
+        //Defines Entry Costs as  .002 ether (~8.75 USD as of 10/12/2021)
+        uint256 Minimum_requirement=.0025 ether;
+        
+        //Defines Lottery Winnings as (~17.50 USD as of 10/12/2021)
+        uint256 Payment_from_Oracle=.005 ether;
+        
+        //Defines lottery states as open, closed,or calculating
+        enum Lottery_state { Open, Closed, Calculating }
+    
+        //Displays the state of the lottery externally
+        Lottery_state public Lottery_Status;
+    
+        //Counts how many players are in the Lottery
+        address payable[] public players;
+    
+        //Counts how many Lotteries have been played
+        uint256 public LotteryId;
+    
+    
+    //sets intial values for variables
+    constructor() public
+    {
+        //Required to connect with Chainlink Oracles
+        setPublicChainlinkToken();
+    
+        //First Lottery
+        LotteryId=1;
+    
+        //Sets Lottery Stateas Closed
+        Lottery_Status=Lottery_state.Closed;
+    }
+    
+    
+    //Function to check if lottery is open and also require payment before adding to Player list
+    function enter() public payable {
+        
+        //Requires Players to pay entry costs
+        assert(msg.value == Minimum_requirement);
+        
+        //Checks that Lottery is open
+        assert(Lottery_Status==Lottery_state.Open);
+        
+        //adds new player to the existing Player list
+        players.push(msg.sender);
+    }
+    
+    
+    //Private Function to pick winner randomly using Chainlink VRFs
+    function PickWinner() private {
+        
+        //Checks that Lottery is Calculating Winner
+        require(Lottery_Status==Lottery_state.Calculating);
+        
+        //Uses Chainlink VRF function in ChainlinkLotteryGovernance.sol to generate randomness
+        VRFRandom(ChainlinkLotteryGovernance.randomness()).getRandom(lotteryId,lotteryId);
+    }
+    
+    
+    //Function to stop player enteries and intiate the PickWinner() function
+    function fulfill_alarm(bytes32 _requestId) 
+        public  
+        recordChainlinkFulfillment(_requestId)
+            {
+                require(Lottery_Status==Lottery_state.Open);
+                Lottery_Status=Lottery_state.Calculating;
+                LotteryId=LotteryId + 1;
+                PickWinner();
+                
+            }
+     
+     
+    //Function to start new lottery and         
+    function start_Lottery(uint256 duration) public {
+        
+        //ensures previous lottery is closed
+        require(Lottery_Status==Lottery_state.Closed);
+        
+        //Starts new lottery
+        Lottery_Status=Lottery_state.Open;
+        
+        //Connects to Chainlink Alarm
+        Chainlink.Request memory req = buildChainlinkRequest(Chainlink_Alarm_JobId, address(this), this.fulfill_alarm.selector);
+        req.addUint("until", now + duration);
+        sendChainlinkRequestTo(Chainlink_Oracle_Alarm,req,Payment_from_Oracle);
+    
+    
+    }
+
+}
